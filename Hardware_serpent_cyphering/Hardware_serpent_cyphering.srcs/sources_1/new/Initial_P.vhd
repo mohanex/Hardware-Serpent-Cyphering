bfbf7fd4146da6ac3b6df1a9fdd53dbc4373093e
mohanex <mohamedpack@gmail.com>
2023-09-08 10:42:30 +0200

    adding vivado project

---
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/Initial_P.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08.09.2023 10:40:03
+-- Design Name: 
+-- Module Name: Initial_P - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Initial_P is
+--  Port ( );
+end Initial_P;
+
+architecture Behavioral of Initial_P is
+
+begin
+
+
+end Behavioral;
