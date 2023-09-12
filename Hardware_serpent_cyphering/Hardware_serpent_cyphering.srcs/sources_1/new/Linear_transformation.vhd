f1e7d3f14e8763bfa830fe27f59563d1db8e93d9
mohanex <mohamedpack@gmail.com>
2023-09-12 15:20:10 +0200

    Adding linear Linear_transformation

---
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/Linear_transformation.vhd
@@ -0,0 +1,23 @@
+----------------------------------------------------------------------------------
+-- Linear transformation
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Linear_transformation is
+    Port ( 
+        clk : in std_logic
+    );
+end Linear_transformation;
+
+architecture Behavioral of Linear_transformation is
+
+begin
+
+
+end Behavioral;
