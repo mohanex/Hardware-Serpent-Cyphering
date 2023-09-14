1f64e22bd29c01e1993815e2d022c58fcdb83160
mohanex <mohamedpack@gmail.com>
2023-09-14 17:29:03 +0200

    Code the linear testbench

--- Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sim_1/new/Linear_transfo_tb.vhd
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sim_1/new/Linear_transfo_tb.vhd
@@ -1,43 +1,73 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 13.09.2023 12:14:37
--- Design Name: 
--- Module Name: Linear_transfo_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Testbench for the linear_transformation module
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
+use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
 entity Linear_transfo_tb is
---  Port ( );
 end Linear_transfo_tb;
 
 architecture Behavioral of Linear_transfo_tb is
 
+component Linear_transformation is
+    generic(
+        constant full_bits : integer :=128;
+        constant div4_bits : integer :=32
+    );
+    Port ( 
+        clk : in std_logic;
+        go : in std_logic;
+        ready_busy : out std_logic;
+        Bi_input : in std_logic_vector(0 to full_bits-1);
+        Bi_output : out std_logic_vector(0 to full_bits-1)
+    );
+end component;
+
+-- constants
+constant const_full_bits : integer :=128;
+constant const_div4_bits : integer :=32
+
+-- clock signals
+signal clk : std_logic := '0' ;
+constant clk_period : time := 20 ns; 
+
+-- componenet signals
+signal s_Bi_input : std_logic_vector(0 to const_full_bits-1);
+signal s_Bi_output : std_logic_vector(0 to const_full_bits-1);
+signal s_go :  std_logic;
+signal s_ready_busy : std_logic;
+
 begin
+    u1 : linear_trans 
+    generic map(
+        full_bits => const_full_bits,
+        div4_bits => const_div4_bits
+    )
+    port map(
+        Bi_input => s_Bi_input,
+        Bi_output => s_Bi_output,
+        clk = > clk,
+        go => s_go,
+        ready_busy => s_ready_busy
+    );
+    clk <= not clk after clk_period/2;
 
+    stimuli : process
+    begin
+        s_go <= '0';
+        s_Bi_input <= (others => '0');
+        
+        wait for 30 ns;
 
+        s_Bi_input <= "0110"; -- should give us 5
+        s_go <= '1';
+        
+        wait for 30 ns;
+    end process;
+    
 end Behavioral;
