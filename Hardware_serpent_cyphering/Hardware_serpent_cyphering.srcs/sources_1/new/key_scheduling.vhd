aaeb0997ceb2526f9158e9aab4bc215031e43036
mohanex <mohamedpack@gmail.com>
2023-09-17 15:01:42 +0200

    adding pdf files

--- Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/key_scheduling.vhd
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/key_scheduling.vhd
@@ -11,22 +11,74 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity key_scheduling is
     generic(
-        constant full_bits : integer :=128;
+        constant full_bits : integer :=256;
         constant div4_bits : integer :=32;
         constant theta : std_logic_vector := X"9e3779b9"
     );
     Port ( 
         clk : in std_logic;
         go : in std_logic;
+        Ki_number : in std_logic;
+        user_key : in std_logic_vector(0 to full_bits-1);
         ready_busy : out std_logic;
-        user_key : in std_logic_vector(0 to 4*full_bits-1);
         Ki : out std_logic_vector(0 to div4_bits-1)
     );
 end key_scheduling;
 
 architecture Behavioral of key_scheduling is
-
+    ----SPLITTING PROCEDURE--------------------------------------
+    procedure  Splitting(
+        L1 : in std_logic_vector(0 to full_bits-1);
+        variable quartet_1 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_2 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_3 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_4 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_5 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_6 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_7 : out std_logic_vector(0 to div4_bits-1);
+        variable quartet_8 : out std_logic_vector(0 to div4_bits-1)
+    )
+    is
+    begin
+        quartet_1 := L1(0 to div4_bits-1);                  -- 00 -> 31
+        quartet_2 := L1((div4_bits) to (2*div4_bits)-1);    -- 32 -> 63
+        quartet_3 := L1((2*div4_bits) to (3*div4_bits)-1);  -- 64 -> 95
+        quartet_4 := L1((3*div4_bits) to (4*div4_bits)-1);  -- 95 -> 127
+        quartet_5 := L1((4*div4_bits) to (5*div4_bits)-1);  -- 128 -> 159
+        quartet_6 := L1((5*div4_bits) to (6*div4_bits)-1);  -- 160 -> 191
+        quartet_7 := L1((6*div4_bits) to (7*div4_bits)-1);  -- 192 -> 223
+        quartet_8 := L1((7*div4_bits) to (8*div4_bits)-1);  -- 224 -> 255
+    end procedure  Splitting;
+    
+    ----MERGING FUCNTION-----------------------------------------
+    function  Merging(
+        quartet_1 : in std_logic_vector(0 to div4_bits-1);
+        quartet_2 : in std_logic_vector(0 to div4_bits-1);
+        quartet_3 : in std_logic_vector(0 to div4_bits-1);
+        quartet_4 : in std_logic_vector(0 to div4_bits-1);
+        quartet_5 : in std_logic_vector(0 to div4_bits-1);
+        quartet_6 : in std_logic_vector(0 to div4_bits-1);
+        quartet_7 : in std_logic_vector(0 to div4_bits-1);
+        quartet_8 : in std_logic_vector(0 to div4_bits-1)
+    )
+    return std_logic_vector is
+        variable tmp1 : std_logic_vector(0 to full_bits-1);
+    begin
+        tmp1(0 to div4_bits-1) := quartet_1;
+        tmp1(div4_bits to (2*div4_bits)-1) := quartet_2;
+        tmp1((2*div4_bits) to (3*div4_bits)-1) := quartet_3;
+        tmp1((3*div4_bits) to (4*div4_bits)-1) := quartet_4;
+        tmp1((4*div4_bits) to (5*div4_bits)-1) := quartet_5;
+        tmp1((5*div4_bits) to (6*div4_bits)-1) := quartet_6;
+        tmp1((6*div4_bits) to (7*div4_bits)-1) := quartet_7;
+        tmp1((7*div4_bits) to (8*div4_bits)-1) := quartet_8;
+        return tmp1;
+    end function  Merging;
 begin
 
+    Scheduling : process(go)
+        begin
+            
+    end process Scheduling;
 
 end Behavioral;
