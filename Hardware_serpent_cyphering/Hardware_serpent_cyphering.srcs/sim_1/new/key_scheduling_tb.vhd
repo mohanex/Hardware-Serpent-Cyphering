312457ef7f465ef043e1f2b018e88e8bd12351ad
mohanex <mohamedpack@gmail.com>
2023-09-22 10:41:29 +0200

    testbench of key_scheduling_tb Version 1

--- Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sim_1/new/key_scheduling_tb.vhd
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sim_1/new/key_scheduling_tb.vhd
@@ -1,43 +1,81 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.09.2023 11:33:35
--- Design Name: 
--- Module Name: key_scheduling_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Key scheduling Testbench
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
+use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
 entity key_scheduling_tb is
---  Port ( );
+
 end key_scheduling_tb;
 
 architecture Behavioral of key_scheduling_tb is
+component key_scheduling 
+    generic(
+        constant full_bits : integer :=128;
+        constant div4_bits : integer :=32;
+        constant full_key_size : integer :=256;
+        constant theta : std_logic_vector := X"9e3779b9";
+        constant four_bits : integer := 4
+    );
+    Port ( 
+        clk : in std_logic;
+        go : in std_logic;
+        Ki_number : in integer; --key number 
+        user_key : in std_logic_vector(0 to full_bits-1);
+        ready_busy : inout std_logic_vector(0 to 1);
+        Ki : out std_logic_vector(0 to div4_bits-1)
+    );
+end component;
+
+--- constants 
+constant full_bits : integer :=128;
+constant div4_bits : integer :=32;
+constant full_key_size : integer :=256;
+constant theta : std_logic_vector := X"9e3779b9";
+constant four_bits : integer := 4
+
+-- clock signals
+signal clk : std_logic := '0' ;
+constant clk_period : time := 20 ns; 
+
+-- componenet signals
+signal sig_go : std_logic;
+signal sig_Ki_number : integer; --key number 
+signal sig_user_key : std_logic_vector(0 to full_bits-1);
+signal sig_ready_busy : std_logic_vector(0 to 1);
+signal sig_Ki : std_logic_vector(0 to div4_bits-1)
 
 begin
 
+key_scheduler : port map(
+    clk => clk,
+    go => sig_go,
+    Ki_number => sig_Ki_number,
+    user_key => sig_user_key,
+    ready_busy => sig_ready_busy,
+    Ki => sig_Ki 
+);
+
+
+clk <= not clk after clk_period/2;
+
+stimuli : process
+begin
+    s_go <= '0';
+    wait for 30 ns;
+
+    sig_user_key <= "01010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101";
+    s_go <= '1';
+    wait for 100 ns;
+
+    sig_Ki_number <='4';
+    wait for 30 ns;
+
+end process;
 
 end Behavioral;
