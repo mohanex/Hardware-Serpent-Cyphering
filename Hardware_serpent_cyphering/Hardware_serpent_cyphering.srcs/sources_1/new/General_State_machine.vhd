6b734b76ed17c25e88b237011c33a3986df9f11a
mohanex <mohamedpack@gmail.com>
2023-09-25 15:25:23 +0200

    adding machine state files

---
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/General_State_machine.vhd
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------------
+-- State machine of permutation
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity General_State_machine is
+--  Port ( );
+end General_State_machine;
+
+architecture Behavioral of General_State_machine is
+
+begin
+
+
+end Behavioral;
---
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/Minor_state_machine.vhd
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------------
+-- State machine of Xoring s-boxing and linear transformation
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Minor_state_machine is
+--  Port ( );
+end Minor_state_machine;
+
+architecture Behavioral of Minor_state_machine is
+
+begin
+
+
+end Behavioral;
