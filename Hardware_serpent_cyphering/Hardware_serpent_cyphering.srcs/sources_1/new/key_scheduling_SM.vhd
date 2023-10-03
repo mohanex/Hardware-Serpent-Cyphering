409f90c711bf52e4325da61828292626dbe47c6c
Afassi Mohamed <afassi.e2106225@etud.univ-ubs.fr>
2023-10-03 20:32:43 +0200

    Modified LT

--- Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/key_scheduling_SM.vhd
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.srcs/sources_1/new/key_scheduling_SM.vhd
@@ -12,6 +12,7 @@ entity key_scheduling_SM is
         constant full_bits : integer :=128;
         constant div4_bits : integer :=32;
         constant full_key_size : integer :=256;
+        --constant theta : std_logic_vector(31 downto 0) := "10011101100111101110110001111001";
         constant theta : std_logic_vector := X"9e3779b9";
         constant four_bits : integer := 4
     );
@@ -19,9 +20,9 @@ entity key_scheduling_SM is
         clk : in std_logic;
         go : in std_logic;
         Ki_number : in integer; --key number 
-        user_key : in std_logic_vector(0 to full_bits-1);
+        user_key : in std_logic_vector(full_bits-1 downto 0);
         ready_busy : out std_logic_vector(0 to 1);
-        Ki : out std_logic_vector(0 to full_bits-1);
+        Ki : out std_logic_vector(full_bits-1 downto 0);
         ready_busy_key : out std_logic_vector(0 to 1)
     );
 end key_scheduling_SM;
@@ -29,22 +30,37 @@ end key_scheduling_SM;
 architecture Behavioral of key_scheduling_SM is
 
     ----TYPES--------------------------------------------------
-    type w_array is array (-8 to 131) of STD_LOGIC_VECTOR(0 to div4_bits-1);
-    type Ki_array is array (0 to 32) of STD_LOGIC_VECTOR(0 to full_bits-1);
+    type w_array is array (-8 to 131) of STD_LOGIC_VECTOR(div4_bits-1 downto 0);
+    type Ki_array is array (0 to 32) of STD_LOGIC_VECTOR(full_bits-1 downto 0);
     type t_sboxes is array (0 to 15) of integer; --S_BOX
     type t_array is array (0 to 127) of integer; --IP
 
+    constant PADDING : std_logic_vector(full_bits-2 downto 0) := (others => '0');
+
     ----SIGNALS--------------------------------------------------
-    signal sig_Ki : std_logic_vector(0 to full_bits-1);
+    signal sig_Ki : std_logic_vector(full_bits-1 downto 0);
     signal sig_Ki_number : integer := 0; --key number 
-    signal sig_user_key : std_logic_vector(0 to full_bits-1);
+    signal sig_user_key : std_logic_vector(full_bits-1 downto 0);
     signal sig_pre_keys : Ki_array;
     signal sig_ready_busy : std_logic_vector(0 to 1);
     signal sig_ready_busy_key : std_logic_vector(0 to 1);
 
+    signal sig_expanded_Key : std_logic_vector(full_key_size-1 downto 0) := (others => '0');
+
+    type state_type is (IDLE, PREKEY_CALCULATION, KEY_EXPEND, ASSEMBLING_KEY, FLAG_IP, MERGING);
+    signal state : state_type := IDLE;
+
+    -- Define signals to control state transitions
+    signal start_processing : std_logic;
+    signal prekey_calc_done : std_logic;
+    signal key_expend_done : std_logic;
+    signal key_assembled : std_logic;
+    signal flag_ip_done : std_logic;
+    signal merging_done : std_logic;
+
     ----S-boxes--------------------------------------------------
     function  app_s_box(
-        input_bits : in std_logic_vector(0 to four_bits-1);
+        input_bits : in std_logic_vector(four_bits-1 downto 0);
         s_box_number : in integer 
     )
     return std_logic_vector is
@@ -57,7 +73,7 @@ architecture Behavioral of key_scheduling_SM is
         variable S6 : t_sboxes :=(7,2,12,5,8,4,6,11,14,9,1,15,13,3,10,0);
         variable S7 : t_sboxes :=(1,13,15,0,14,8,2,11,7,4,12,10,9,3,5,6);
 
-        variable tmp1 : std_logic_vector(0 to four_bits-1);
+        variable tmp1 : std_logic_vector(four_bits-1 downto 0);
         variable read_value_in : integer;
         variable read_value_out : integer;
     begin
@@ -93,10 +109,21 @@ architecture Behavioral of key_scheduling_SM is
         return tmp1;
     end function  app_s_box;
 
+    ----RV  -------------------------------------------------------
+    function RV (input_vector: in std_logic_vector)
+    return std_logic_vector is
+        variable reversed_result: std_logic_vector(input_vector'RANGE);
+        alias reversed_alias: std_logic_vector(input_vector'REVERSE_RANGE) is input_vector;
+    begin
+        for index in reversed_alias'RANGE loop
+            reversed_result(index) := reversed_alias(index);
+        end loop;
+        return reversed_result;
+    end;
 
     ----IP-------------------------------------------------------
     function  app_IP(
-        input_bits : in std_logic_vector(0 to full_bits-1)
+        input_bits : in std_logic_vector(full_bits-1 downto 0)
     )
     return std_logic_vector is
         variable ip_table  : t_array := (
@@ -109,11 +136,11 @@ architecture Behavioral of key_scheduling_SM is
             24, 56, 88, 120, 25, 57, 89, 121, 26, 58, 90, 122, 27, 59, 91, 123,
             28, 60, 92, 124, 29, 61, 93, 125, 30, 62, 94, 126, 31, 63, 95, 127
             );
-        variable tmp1 : std_logic_vector(0 to full_bits-1);
+        variable tmp1 : std_logic_vector(full_bits-1 downto 0);
         variable compt : integer := 0;
         variable temp : integer;
     begin
-        for compt in 0 to full_bits-1 loop
+        for compt in full_bits-1 downto 0 loop
             temp := ip_table(compt);
             tmp1(temp) := input_bits(compt);
         end loop;
@@ -123,95 +150,123 @@ architecture Behavioral of key_scheduling_SM is
     ----SPLITTING PROCEDURE--------------------------------------
     procedure  Splitting(
         L1 : in std_logic_vector(0 to full_key_size-1);
-        variable var_quartet_1 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_2 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_3 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_4 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_5 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_6 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_7 : out std_logic_vector(0 to div4_bits-1);
-        variable var_quartet_8 : out std_logic_vector(0 to div4_bits-1)
+        variable var_quartet_1 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_2 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_3 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_4 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_5 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_6 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_7 : out std_logic_vector(div4_bits-1 downto 0);
+        variable var_quartet_8 : out std_logic_vector(div4_bits-1 downto 0)
     )
     is
     begin
-        var_quartet_1 := L1(0 to div4_bits-1);                  -- 00 -> 31
-        var_quartet_2 := L1((div4_bits) to (2*div4_bits)-1);    -- 32 -> 63
-        var_quartet_3 := L1((2*div4_bits) to (3*div4_bits)-1);  -- 64 -> 95
-        var_quartet_4 := L1((3*div4_bits) to (4*div4_bits)-1);  -- 95 -> 127
-        var_quartet_5 := L1((4*div4_bits) to (5*div4_bits)-1);  -- 128 -> 159
-        var_quartet_6 := L1((5*div4_bits) to (6*div4_bits)-1);  -- 160 -> 191
-        var_quartet_7 := L1((6*div4_bits) to (7*div4_bits)-1);  -- 192 -> 223
-        var_quartet_8 := L1((7*div4_bits) to (8*div4_bits)-1);  -- 224 -> 255
+        var_quartet_8 := L1(full_key_size-1 downto full_key_size-div4_bits);
+        var_quartet_7 := L1(full_key_size-div4_bits-1 downto full_key_size-2*div4_bits);
+        var_quartet_6 := L1(full_key_size-2*div4_bits-1 downto full_key_size-3*div4_bits);
+        var_quartet_5 := L1(full_key_size-3*div4_bits-1 downto full_key_size-4*div4_bits);
+        var_quartet_4 := L1(full_key_size-4*div4_bits-1 downto full_key_size-5*div4_bits);
+        var_quartet_3 := L1(full_key_size-5*div4_bits-1 downto full_key_size-6*div4_bits);
+        var_quartet_2 := L1(full_key_size-6*div4_bits-1 downto full_key_size-7*div4_bits);
+        var_quartet_1 := L1(full_key_size-7*div4_bits-1 downto full_key_size-8*div4_bits);
     end procedure  Splitting;
     
     ----MERGING FUNCTION-----------------------------------------
-    function  Merging(
-        quartet_1 : in std_logic_vector(0 to div4_bits-1);
-        quartet_2 : in std_logic_vector(0 to div4_bits-1);
-        quartet_4 : in std_logic_vector(0 to div4_bits-1);
-        quartet_3 : in std_logic_vector(0 to div4_bits-1)
+    function  Merging2(
+        quartet_1 : in std_logic_vector(div4_bits-1 downto 0);
+        quartet_2 : in std_logic_vector(div4_bits-1 downto 0);
+        quartet_4 : in std_logic_vector(div4_bits-1 downto 0);
+        quartet_3 : in std_logic_vector(div4_bits-1 downto 0)
     )
     return std_logic_vector is
-        variable tmp1 : std_logic_vector(0 to full_bits-1);
+        variable tmp1 : std_logic_vector(full_bits-1 downto 0);
     begin
-        tmp1(0 to div4_bits-1) := quartet_1;
-        tmp1(div4_bits to (2*div4_bits)-1) := quartet_2;
-        tmp1((2*div4_bits) to (3*div4_bits)-1) := quartet_3;
-        tmp1((3*div4_bits) to (4*div4_bits)-1) := quartet_4;
+        tmp1((4*div4_bits)-1 downto (3*div4_bits)) := quartet_1;
+        tmp1((3*div4_bits)-1 downto (2*div4_bits)) := quartet_2;
+        tmp1((2*div4_bits)-1 downto div4_bits) := quartet_3;
+        tmp1(div4_bits-1 downto 0) := quartet_4;
         return tmp1;
-    end function  Merging;
-    
-    ----ROTATING FUNCTION--------------------------------------
-    function Rotating(
-        L1 : in std_logic_vector(0 to div4_bits-1);
-        rotating_amount : in integer
-    )
-    return std_logic_vector is
-        variable tmp1 : std_logic_vector(0 to div4_bits-1);
-        variable tmp2 : std_logic_vector(0 to rotating_amount-1);
-    begin
-        if rotating_amount >= 0 and rotating_amount <= 31 then
-            tmp2 := L1(0 to rotating_amount-1);
-            tmp1(0 to ((div4_bits-rotating_amount)-1) ) := L1(rotating_amount to div4_bits-1);
-            tmp1((div4_bits-rotating_amount) to div4_bits-1) := tmp2;
-        else 
-            tmp1 := (others => '0');
-        end if;
-        return tmp1;
-    end function Rotating;
-
-    ----SHIFTING FUNCTION ---------------------------------
-    function Shifting(
-        L1 : in std_logic_vector(0 to div4_bits-1);
-        shift_amount : in integer
-    )
-    return std_logic_vector is
-        variable tmp1 : std_logic_vector(0 to div4_bits-1);
-        constant ZERO : std_logic_vector(0 to shift_amount-1) := (others => '0');
-    begin
-        if shift_amount >= 0 and shift_amount <= 31 then
-            tmp1(0 to ((div4_bits-shift_amount)-1) ) := L1(shift_amount to div4_bits-1);
-            tmp1((div4_bits-shift_amount) to div4_bits-1) := ZERO;
-        else  
-            tmp1 := (others => '1');
-        end if;
-        return tmp1;
-    end function Shifting;
--- Define states for the state machine
-type state_type is (IDLE, KEY_PADDING, SPLIT_KEY, PREKEY_CALCULATION, SBOX_APPLICATION, ASSEMBLING_KEY, FLAG_IP, FINAL_IP);
-signal state : state_type := IDLE;
-
--- Define signals to control state transitions
-signal start_processing : std_logic;
-signal padding_done : std_logic;
-signal split_key_done : std_logic;
-signal prekey_calc_done : std_logic;
-signal sbox_app_done : std_logic;
-signal key_assembled : std_logic;
-signal final_ip_done : std_logic;
-signal flag_ip_done : std_logic;
+    end function  Merging2;
 
 begin
+
+    sig_expanded_Key <= RV(sig_user_key) & '1' & PADDING;
+
+    process(state)
+        variable w : w_array := (others => (others => '0'));
+        variable k : w_array := (others => (others => '0'));
+        variable temp1 : std_logic_vector(div4_bits-1 downto 0);
+        variable input_s : std_logic_vector (four_bits-1 downto 0);
+        variable output_s : std_logic_vector (four_bits-1 downto 0);
+        variable i : integer := 0;
+        variable j : integer := 0;
+        variable pre_keys : Ki_array;
+        variable whichS : integer;
+    begin
+    if rising_edge(clk) then
+        case state is
+        when IDLE =>
+            --report "IDLE State";
+            start_processing <= '1';
+
+        when PREKEY_CALCULATION =>
+        Splitting(L1=>sig_expanded_Key,var_quartet_1=>w(-1),var_quartet_2=>w(-2),var_quartet_3=>w(-3),
+        var_quartet_4=>w(-4),var_quartet_5=>w(-5),var_quartet_6=>w(-6),var_quartet_7=>w(-7),
+        var_quartet_8=>w(-8));
+        prekey_calc_done <= '1';
+
+        when KEY_EXPEND =>
+        for i in 0 to 131 loop
+            temp1 := w(i-8) xor w(i-5) xor w(i-3) xor w(i-1) xor theta xor RV(std_logic_vector(to_unsigned(i,32)));
+            w(i) := std_logic_vector(rotate_right(unsigned(temp1), 11));
+        end loop;
+        key_expend_done <='1';
+
+        when ASSEMBLING_KEY =>
+        applying_key_sbox : for i in 0 to 32 loop
+            whichS := (32 + 3 - i) mod 32;
+            for j in 0 to 31 loop
+                -- Extract individual bits from w
+                input_s := w(0 + 4 * i)(31-j) & w(1 + 4 * i)(31-j) & w(2 + 4 * i)(31-j) & w(3 + 4 * i)(31-j);
+                --report "The value of 'input_s' is " & integer'image(to_integer(unsigned(input_s)));
+                -- Call S box function
+                output_s := app_s_box(input_bits=>input_s,s_box_number=>whichS mod 8);
+                for l in 0 to 3 loop
+                    --k(full_bits-l*div4_bits-1-j) := output_s(3-l);
+                    k(4 * i + l) := std_logic_vector(shift_right(unsigned(k(4 * i + l)), 1));
+                    k(4 * i + l)(div4_bits-1) := output_s(l);
+                end loop;
+            end loop;
+        end loop;
+        if i=32 then
+            key_assembled <='1';
+        end if;
+
+        when MERGING =>
+            merging_ki : for i in 0 to 32 loop
+                pre_keys(i) := Merging2(quartet_1=>k(4*i),quartet_2=>k(4*i+1),quartet_3=>k(4*i+2),quartet_4=>k(4*i+3));
+            end loop;
+            if i = 32 then
+                for j in 0 to 32 loop
+                    pre_keys(j) := app_IP(input_bits=>pre_keys(j));
+                end loop;
+            end if;
+            if j=32 then
+                merging_done <='1';
+            end if;
+        
+        when FLAG_IP =>
+            sig_pre_keys <= pre_keys;
+            flag_ip_done <= '1';
+
+        when others =>
+            report "seg fault";
+        end case;
+    end if;
+    end process;
+
+
+
 -- State machine process
 process(clk, go)
 begin
@@ -219,32 +274,24 @@ begin
             case state is
                 when IDLE =>
                     if start_processing = '1' and go = '1' then
-                        state <= KEY_PADDING;
-                    end if;
-                when KEY_PADDING =>
-                    if padding_done = '1' then
-                        state <= SPLIT_KEY;
-                    end if;
-                when SPLIT_KEY =>
-                    if split_key_done = '1' then
                         state <= PREKEY_CALCULATION;
                     end if;
+
                 when PREKEY_CALCULATION =>
                     if prekey_calc_done = '1' then
-                        state <= SBOX_APPLICATION;
-                    end if;
-                when SBOX_APPLICATION =>
-                    if sbox_app_done = '1' then
                         state <= ASSEMBLING_KEY;
                     end if;
+
                 when ASSEMBLING_KEY =>
                     if key_assembled = '1' then
-                        state <= FINAL_IP;
+                        state <= MERGING;
                     end if;
-                when FINAL_IP =>
-                    if final_ip_done = '1' then
+
+                when MERGING =>
+                    if merging_done = '1' then
                         state <= FLAG_IP;
                     end if;
+
                 when FLAG_IP =>
                     if flag_ip_done = '1' and go = '1' then
                         state <= FLAG_IP;
@@ -256,112 +303,6 @@ begin
                     state <= IDLE;
             end case;
     end if;
-end process;
-
--- State machine logic to set the next state
-process(state)
-variable key_to_256 : std_logic_vector(0 to full_key_size-1);
-variable padding_zeros : std_logic_vector(0 to 126);
-variable temp_calc : std_logic_vector (0 to div4_bits-1);
-variable input_s : std_logic_vector (0 to four_bits-1);
-variable output_s : std_logic_vector (0 to four_bits-1);
-
-variable w : w_array;
-variable k : w_array;
-variable pre_keys : Ki_array;
-
-variable whichS : integer;
-variable i : integer := 0;
-variable padding_number : integer := 0;
-begin
-    case state is
-        when IDLE =>
-            --report "IDLE State";
-            start_processing <= '1';
-            --sig_ready_busy <= "10";
-
-        when KEY_PADDING =>
-            --report "KEY_PADDING State";
-            if(padding_number = 0) then
-                padding_zeros := (others => '1');
-                key_to_256(full_key_size-1) := '1';
-                key_to_256(127 to 254) := sig_user_key;
-                key_to_256(0 to 126) := padding_zeros;
-                padding_number := 1;
-            end if;
-            padding_done <= '1';
-
-        when SPLIT_KEY =>
-        --report "SPLIT_KEY State";
-        Splitting(L1=>key_to_256,var_quartet_1=>w(-8),var_quartet_2=>w(-7),var_quartet_3=>w(-6),
-        var_quartet_4=>w(-5),var_quartet_5=>w(-4),var_quartet_6=>w(-3),var_quartet_7=>w(-2),
-        var_quartet_8=>w(-1));
-        split_key_done <= '1';
-
-        when PREKEY_CALCULATION =>
-        --report "PREKEY_CALCULATION State";
-        prekey : for i in 0 to 131 loop
-            temp_calc := w(i-8) xor w(i-5) xor w(i-3) xor w(i-1) xor theta xor 
-            std_logic_vector(to_unsigned(i,32));
-            report "Prekey calculation, iteration number :" & integer'image(i);
-            report "The value of 'w(i)' before rotation :" & integer'image(to_integer(unsigned(temp_calc)));
-            w(i) := Rotating(L1=>temp_calc,rotating_amount=>11);
-            report "The value of 'w(i)' after rotation :" & integer'image(to_integer(unsigned(w(i))));
-        end loop;
-        prekey_calc_done <= '1';
-
-        when SBOX_APPLICATION =>
-        --report "SBOX_APPLICATION State";
-        applying_key_sbox : for i in 0 to 32 loop
-            whichS := (32 + 3 - i) mod 32;
-            for j in 0 to 31 loop
-                -- Extract individual bits from w
-                input_s := w(0 + 4 * i)(j) & w(1 + 4 * i)(j) & w(2 + 4 * i)(j) & w(3 + 4 * i)(j);
-                --report "The value of 'input_s' is " & integer'image(to_integer(unsigned(input_s)));
-                -- Call S box function
-                output_s := app_s_box(input_bits=>input_s,s_box_number=>whichS);
-                for l in 0 to 3 loop
-                    k(4 * i + l) := Shifting(L1=>k(4 * i + l),shift_amount=>1);
-                    k(4 * i + l)(div4_bits-1) := output_s(l);
-                end loop;
-            end loop;
-        end loop;
-        sbox_app_done <= '1';
-
-        when ASSEMBLING_KEY =>
-        --report "ASSEMBLING_KEY State";
-        assembling_key2 : for i in 0 to 32 loop
-            pre_keys(i) := Merging(quartet_1=>k(4*i),quartet_2=>k(4*i+1),quartet_3=>k(4*i+2),quartet_4=>k(4*i+3));
-        end loop;
-        key_assembled <= '1';
-
-        when FINAL_IP =>
-        --report "FINAL_IP State";
-        applying_ip : for i in 0 to 32 loop
-            pre_keys(i) := app_IP(input_bits=>pre_keys(i));
-            sig_pre_keys(i) <= pre_keys(i);
-        end loop;
-        final_ip_done <= '1';
-        flag_ip_done <= '0';
-
-        when FLAG_IP =>
-        --report "FLAG_IP State";
-        --sig_ready_busy <= "11";
-        padding_number := 0;
-        ------ reset control signals -----
-        start_processing <= '0';
-        padding_done <= '0';
-        split_key_done <= '0';
-        prekey_calc_done <= '0';
-        sbox_app_done <= '0';
-        key_assembled <= '0';
-        final_ip_done <= '0';
-        flag_ip_done <= '1';
-        
-        when others =>
-        report "others State";
-        --sig_ready_busy <= "00";
-    end case;
 end process;
 
     Giving_keys : process(clk,sig_Ki_number,state)
