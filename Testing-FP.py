87069fbcb18c9d2495f3ab7389c7ee64805e8e7d
Afassi Mohamed <afassi.e2106225@etud.univ-ubs.fr>
2023-10-10 23:31:46 +0200

    vals

--- Testing-FP.py
+++ Testing-FP.py
@@ -1,6 +1,7 @@
 from serpent_alg import * 
 
-val1 = input("enter hex value to Permutate : ")
+val2 = input("enter hex value to Permutate : ")
 
-valout = FP(hexstring2bitstring(val1))
+valout = FP(hexstring2bitstring(val2))
+
 print(bitstring2hexstring(valout))
