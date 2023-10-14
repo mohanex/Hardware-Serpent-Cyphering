824b8b46b31efda4ade3a9c1523604da7f51b1aa
Afassi Mohamed <afassi.e2106225@etud.univ-ubs.fr>
2023-10-14 12:03:32 +0200

    changing file repo

--- Hardware_serpent_cyphering/Hardware_serpent_cyphering.sim/sim_1/behav/xsim/xsim.dir/Minor_SM_tb_behav/obj/xsim_1.c
+++ Hardware_serpent_cyphering/Hardware_serpent_cyphering.sim/sim_1/behav/xsim/xsim.dir/Minor_SM_tb_behav/obj/xsim_1.c
@@ -74,8 +74,8 @@ const int NumRelocateId= 24;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/Minor_SM_tb_behav/xsim.reloc",  (void **)funcTab, 24);
-	iki_vhdl_file_variable_register(dp + 18840);
-	iki_vhdl_file_variable_register(dp + 18896);
+	iki_vhdl_file_variable_register(dp + 18888);
+	iki_vhdl_file_variable_register(dp + 18944);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
